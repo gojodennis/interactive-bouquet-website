41659f3a782858dc7971903f9003eecb58bd1dac
gojodennis <107945689+gojodennis@users.noreply.github.com>
2025-02-03 00:02:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
   assetPrefix: '/interactive-bouquet-website/',
   basePath: '/interactive-bouquet-website',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig;
